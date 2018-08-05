bc9c4f04cce88eff318e8e46f80b6b35c9fa36ba
VeselovaAnna <Lavrav@ukr.net>
2018-08-05 18:57:51 +0300

    ДЗ-1

---
+++ Homework1.java
@@ -0,0 +1,9 @@
+public class Homework1 {
+    public static void main(String[] args) {
+        String name = "Привет всем";
+        String name1 = "Я была на море летом 2017";
+  System.out.println (name);
+        System.out.println (name1);
+        System. out. println("Я была на море летом 2017");
+    }
+}
